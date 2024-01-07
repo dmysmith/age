032d501600de8471499fce4a32885312286d2e35
Diana Smith <dmysmith@gmail.com>
2024-01-07 12:39:49 -0800

    bug fix in run_makeDesign

--- s2_makeDesign.R
+++ s2_makeDesign.R
@@ -18,9 +18,12 @@ ndafile <- '/space/syn50/1/data/ABCD/d9smith/age/nda5.0_bfs.txt'
 nda <- read.delim(ndafile)
 
 # only include subjects which pass QC
-# TODO: currently this variable does not have 4 year data? once this is fixed remove the hack to add everyone from y4
 idx_dmri_inc <- which(nda$imgincl_dmri_include==1|nda$eventname=='4_year_follow_up_y_arm_1')
 nda_dmri_inc <- nda[idx_dmri_inc,]
+
+# males and females separately
+nda_dmri_inc_male <- nda_dmri_inc[nda_dmri_inc$sex=='M',]
+nda_dmri_inc_female <- nda_dmri_inc[nda_dmri_inc$sex=='F',]
 
 
 # df for subjects with data for all three timepoints
@@ -32,7 +35,7 @@ nda_dmri_inc_completecases <- nda_dmri_inc[complete_idx,]
 
 
 # Define the path to the directory where you would like to save out your design matrix 
-outpath <- '/space/syn50/1/data/ABCD/d9smith/age/results_2023-12-11'
+outpath <- '/space/syn50/1/data/ABCD/d9smith/age/results_2024-01-06'
 
 ###############################################################
 # Design Matrix 1: Replicating model from Clare's paper (baseline and y2 only)
@@ -120,7 +123,50 @@ catvar <- c('sex', 'high.educ', 'hisp', 'mri_info_deviceserialnumber', 'mri_info
 time <- c('baseline_year_1_arm_1', '2_year_follow_up_y_arm_1', '4_year_follow_up_y_arm_1')
 
 # Note that default is set to demean=TRUE (demean continuous variables)
-makeDesign(nda, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
+makeDesign(nda_dmri_inc, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
+
+############################################################
+# Design Matrix 4f: basis functions, all timepoints, females only
+fname <- 'designMat4f_BFsSexIncEducHispPCsScanSoftMotion_bly2y4.txt'
+outfile <- paste0(outpath, '/', fname) 
+
+# fixed effects: age, sex, household income, parental education, Hispanic ethnicity 
+# top 10 genetic principal components, scanner ID, MRI software version, 
+# motion (average frame-wise displacement in mm)
+
+# Continuous variables
+contvar <- c(paste0('bf_demean_',1:4), 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8','PC9', 'PC10', 'dmri_meanmotion', 'household.income_cont')
+
+# Categorical variables
+catvar <- c('high.educ', 'hisp', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+
+# The time points for which we wish to extract data are specified; specify in chronoligical order
+time <- c('baseline_year_1_arm_1', '2_year_follow_up_y_arm_1', '4_year_follow_up_y_arm_1')
+
+# Note that default is set to demean=TRUE (demean continuous variables)
+makeDesign(nda_dmri_inc_female, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
+
+############################################################
+# Design Matrix 4m: basis functions, all timepoints, males only
+fname <- 'designMat4m_BFsSexIncEducHispPCsScanSoftMotion_bly2y4.txt'
+outfile <- paste0(outpath, '/', fname) 
+
+# fixed effects: age, sex, household income, parental education, Hispanic ethnicity 
+# top 10 genetic principal components, scanner ID, MRI software version, 
+# motion (average frame-wise displacement in mm)
+
+# Continuous variables
+contvar <- c(paste0('bf_demean_',1:4), 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8','PC9', 'PC10', 'dmri_meanmotion', 'household.income_cont')
+
+# Categorical variables
+catvar <- c('high.educ', 'hisp', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+
+# The time points for which we wish to extract data are specified; specify in chronoligical order
+time <- c('baseline_year_1_arm_1', '2_year_follow_up_y_arm_1', '4_year_follow_up_y_arm_1')
+
+# Note that default is set to demean=TRUE (demean continuous variables)
+makeDesign(nda_dmri_inc_male, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
+
 
 ############################################################
 # Design Matrix 5: basis functions, all timepoints, plus PDS
@@ -141,4 +187,46 @@ catvar <- c('sex', 'high.educ', 'hisp', 'mri_info_deviceserialnumber', 'mri_info
 time <- c('baseline_year_1_arm_1', '2_year_follow_up_y_arm_1', '4_year_follow_up_y_arm_1')
 
 # Note that default is set to demean=TRUE (demean continuous variables)
-makeDesign(nda, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
+makeDesign(nda_dmri_inc, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
+
+############################################################
+# Design Matrix 5f: basis functions, all timepoints, plus PDS, females only
+fname <- 'designMat5f_BFsSexIncEducHispPCsScanSoftMotionPDS_bly2y4.txt'
+outfile <- paste0(outpath, '/', fname) 
+
+# fixed effects: age, sex, household income, parental education, Hispanic ethnicity 
+# top 10 genetic principal components, scanner ID, MRI software version, 
+# motion (average frame-wise displacement in mm)
+
+# Continuous variables
+contvar <- c(paste0('bf_demean_',1:4), 'pds_avg', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8','PC9', 'PC10', 'dmri_meanmotion', 'household.income_cont')
+
+# Categorical variables
+catvar <- c('high.educ', 'hisp', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+
+# The time points for which we wish to extract data are specified; specify in chronoligical order
+time <- c('baseline_year_1_arm_1', '2_year_follow_up_y_arm_1', '4_year_follow_up_y_arm_1')
+
+# Note that default is set to demean=TRUE (demean continuous variables)
+makeDesign(nda_dmri_inc_female, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
+
+############################################################
+# Design Matrix 5m: basis functions, all timepoints, plus PDS, males only
+fname <- 'designMat5m_BFsSexIncEducHispPCsScanSoftMotionPDS_bly2y4.txt'
+outfile <- paste0(outpath, '/', fname) 
+
+# fixed effects: age, sex, household income, parental education, Hispanic ethnicity 
+# top 10 genetic principal components, scanner ID, MRI software version, 
+# motion (average frame-wise displacement in mm)
+
+# Continuous variables
+contvar <- c(paste0('bf_demean_',1:4), 'pds_avg', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8','PC9', 'PC10', 'dmri_meanmotion', 'household.income_cont')
+
+# Categorical variables
+catvar <- c('high.educ', 'hisp', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+
+# The time points for which we wish to extract data are specified; specify in chronoligical order
+time <- c('baseline_year_1_arm_1', '2_year_follow_up_y_arm_1', '4_year_follow_up_y_arm_1')
+
+# Note that default is set to demean=TRUE (demean continuous variables)
+makeDesign(nda_dmri_inc_male, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
