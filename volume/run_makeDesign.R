71db5f276486c4a71c1ec3cdbef43ccba641dad1
Diana Smith <dmysmith@gmail.com>
2024-04-08 10:14:57 -0700

    recode X variables so all are mean 0

--- volume/run_makeDesign.R
+++ volume/run_makeDesign.R
@@ -6,7 +6,7 @@
 
 ###############################################################
 # install packages
-for (p in c("plyr")){
+for (p in c("plyr", "dplyr")){
         if(!eval(parse(text=paste("require(",p,")")))) {
                 install.packages(p)
                 lapply(p,library,character.only=TRUE)
@@ -36,14 +36,22 @@ nda6_t1w_inc <- nda6[idx_t1w_inc,]
 vars_interest = c('src_subject_id', 'eventname', 'rel_family_id', 'interview_age', 
                 paste0('PC',1:10),'household.income_cont','sex', 'high.educ', 
                 'mri_info_deviceserialnumber', 'mri_info_softwareversion',
-                'pds_y_p_average', names(nda)[grep('bf_',names(nda))])
+                'pds_y_p_average', names(nda6)[grep('bf_',names(nda6))])
 
-# nda_t1w_inc_completecases = na.omit(nda_t1w_inc[,vars_interest]) # NB: this includes two intersex subjects
-# nda_t1w_inc_completemales = nda_t1w_inc_completecases[nda_t1w_inc_completecases$sex=='M',]
-# nda_t1w_inc_completefemales = nda_t1w_inc_completecases[nda_t1w_inc_completecases$sex=='F',]
-
-# nda6_t1w_inc_completecases = na.omit(nda6_t1w_inc[,names(nda6) %in% vars_interest])
+# create dataframe with complete cases only
 nda6_t1w_inc_completecases = na.omit(nda6_t1w_inc[,vars_interest]) 
+
+# create variable sex_cont where average is 0, and variable sex_I whos value is 1 for intersex individuals
+nda6_t1w_inc_completecases$sex_cont = recode(nda6_t1w_inc_completecases$sex, F = -0.5, I = 0, M = 0.5)
+
+# create scanner variables where reference is the most common value
+nda6_t1w_inc_completecases$mri_info_softwareversion = relevel(as.factor(nda6_t1w_inc_completecases$mri_info_softwareversion), ref = "syngo MR E11")
+nda6_t1w_inc_completecases$mri_info_deviceserialnumber = relevel(as.factor(nda6_t1w_inc_completecases$mri_info_deviceserialnumber), ref = "HASH3935c89e")
+
+# recode high.educ sp reference is the most common value
+nda6_t1w_inc_completecases$high.educ = relevel(as.factor(nda6_t1w_inc_completecases$high.educ), ref = "Some College")
+
+# create separate dataframes for males and females (for running sex stratified models)
 nda6_t1w_inc_completemales = nda6_t1w_inc_completecases[nda6_t1w_inc_completecases$sex=='M',]
 nda6_t1w_inc_completefemales = nda6_t1w_inc_completecases[nda6_t1w_inc_completecases$sex=='F',]
 
@@ -53,10 +61,10 @@ fname <- 'designmat1_SAgeSexScanSoft.txt'
 outfile <- paste0(outpath, '/', fname) 
 
 # Continuous variables
-contvar <- c(names(nda6)[grep('bf_demean',names(nda6))])
+contvar <- c(names(nda6)[grep('bf_demean',names(nda6))], 'sex_cont')
 
 # Categorical variables
-catvar <- c('sex', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+catvar <- c('mri_info_deviceserialnumber', 'mri_info_softwareversion')
 
 # The time points for which we wish to extract data are specified; specify in chronoligical order
 time <- c('baseline_year_1_arm_1', '2_year_follow_up_y_arm_1', '4_year_follow_up_y_arm_1', '6_year_follow_up_y_arm_1')
@@ -68,14 +76,14 @@ makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=ca
 # Design Matrix 1m: brain ~ s(age) + scan + soft [MALES]
 fname <- 'designmat1m_SAgeSexScanSoft_males.txt'
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('mri_info_deviceserialnumber', 'mri_info_softwareversion') 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
 # Design Matrix 1f: brain ~ s(age) + scan + soft [FEMALES]
 fname <- 'designmat1f_SAgeSexScanSoft_females.txt'
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('mri_info_deviceserialnumber', 'mri_info_softwareversion')  
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completefemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
@@ -84,10 +92,10 @@ fname <- 'designmat2_SAgeSexScanSoftPCs.txt'
 outfile <- paste0(outpath, '/', fname) 
 
 # Continuous variables
-contvar <- c(names(nda6)[grep('bf_demean',names(nda6))], paste0('PC',1:10))
+contvar <- c(names(nda6)[grep('bf_demean',names(nda6))], 'sex_cont', paste0('PC',1:10))
 
 # Categorical variables
-catvar <- c('sex', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+catvar <- c('mri_info_deviceserialnumber', 'mri_info_softwareversion')
 
 # Note that default is set to demean=TRUE (demean continuous variables)
 makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
@@ -96,14 +104,14 @@ makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=ca
 # Design Matrix 2m: brain ~ s(age) + sex + scan + soft + PCs  [MALES]
 fname <- 'designmat2m_SAgeSexScanSoftPCs_males.txt'
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('mri_info_deviceserialnumber', 'mri_info_softwareversion') 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
 # Design Matrix 2f: brain ~ s(age) + sex + scan + soft + PCs  [FEMALES]
 fname <- 'designmat2f_SAgeSexScanSoftPCs_females.txt'
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('mri_info_deviceserialnumber', 'mri_info_softwareversion')  
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completefemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
@@ -112,10 +120,10 @@ fname <- 'designmat3_SAgeSexScanSoftIncEduc.txt'
 outfile <- paste0(outpath, '/', fname) 
 
 # Continuous variables
-contvar <- c(names(nda6)[grep('bf_demean',names(nda6))], 'household.income_cont')
+contvar <- c(names(nda6)[grep('bf_demean',names(nda6))], 'sex_cont', 'household.income_cont')
 
 # Categorical variables
-catvar <- c('sex', 'high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
 
 # Note that default is set to demean=TRUE (demean continuous variables)
 makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
@@ -124,14 +132,14 @@ makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=ca
 # Design Matrix 3m: brain ~ s(age) + sex + scan + soft + inc + educ [MALES]
 fname <- 'designmat3m_SAgeSexScanSoftIncEduc_males.txt'
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion') 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
 # Design Matrix 3f: brain ~ s(age) + sex + scan + soft + inc + educ [FEMALES]
 fname <- 'designmat3f_SAgeSexScanSoftIncEduc_females.txt'
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')  
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completefemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
@@ -140,10 +148,10 @@ fname <- 'designmat4_SAgeSexScanSoftIncEducPCs.txt'
 outfile <- paste0(outpath, '/', fname) 
 
 # Continuous variables
-contvar <- c(names(nda6)[grep('bf_demean',names(nda6))], paste0('PC',1:10), 'household.income_cont')
+contvar <- c(names(nda6)[grep('bf_demean',names(nda6))], 'sex_cont', paste0('PC',1:10), 'household.income_cont')
 
 # Categorical variables
-catvar <- c('sex', 'high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
 
 # Note that default is set to demean=TRUE (demean continuous variables)
 makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
@@ -152,14 +160,14 @@ makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=ca
 # Design Matrix 4m: brain ~ s(age) + sex + scan + soft + inc + educ + PCs [MALES]
 fname <- 'designmat4m_SAgeSexScanSoftIncEducPCs_males.txt'
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion') 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
 # Design Matrix 4f: brain ~ s(age) + sex + scan + soft + inc + educ + PCs [FEMALES]
 fname <- 'designmat4f_SAgeSexScanSoftIncEducPCs_females.txt'
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')  
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completefemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
@@ -168,10 +176,10 @@ fname <- 'designmat5_SAgeSexScanSoftIncEducPCsSPDSavg.txt'
 outfile <- paste0(outpath, '/', fname) 
 
 # Continuous variables
-contvar <- c(names(nda)[grep('bf_demean',names(nda))], names(nda)[grep('bf_pdsavg',names(nda))], paste0('PC',1:10), 'household.income_cont')
+contvar <- c(names(nda6)[grep('bf_demean',names(nda6))], 'sex_cont', names(nda6)[grep('bf_pdsavg',names(nda6))], paste0('PC',1:10), 'household.income_cont')
 
 # Categorical variables
-catvar <- c('sex', 'high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
 
 # Note that default is set to demean=TRUE (demean continuous variables)
 makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
@@ -180,14 +188,14 @@ makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=ca
 # Design Matrix 5m: brain ~ s(age) + sex + scan + soft + inc + educ + PCs + s(PDS) [MALES]
 fname <- 'designmat5m_SAgeSexScanSoftIncEducPCsSPDSavg_males.txt' 
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion') 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
 # Design Matrix 5f: brain ~ s(age) + sex + scan + soft + inc + educ + PCs + s(PDS) [FEMALES]
 fname <- 'designmat5f_SAgeSexScanSoftIncEducPCsSPDSavg_females.txt' 
 outfile <- paste0(outpath, '/', fname) 
-catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion') 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completefemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
@@ -196,10 +204,10 @@ fname <- 'designmat6_SAgeSexScanSoftIncEducPCsSBMI.txt'
 outfile <- paste0(outpath, '/', fname) 
 
 # Continuous variables
-contvar <- c(names(nda)[grep('bf_demean',names(nda))], names(nda)[grep('bf_bmi',names(nda))], paste0('PC',1:10), 'household.income_cont')
+contvar <- c(names(nda6)[grep('bf_demean',names(nda6))], 'sex_cont', names(nda6)[grep('bf_bmi',names(nda6))], paste0('PC',1:10), 'household.income_cont')
 
 # Categorical variables
-catvar <- c('sex', 'high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
 
 # Note that default is set to demean=TRUE (demean continuous variables)
 makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
@@ -208,40 +216,68 @@ makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=ca
 # Design Matrix 6m: brain ~ s(age) + sex + scan + soft + inc + educ + PCs + s(BMI) [MALES]
 fname <- 'designmat6m_SAgeSexScanSoftIncEducPCsSBMI_males.txt' 
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion') 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
 # Design Matrix 6f: brain ~ s(age) + sex + scan + soft + inc + educ + PCs + s(BMI) [FEMALES]
 fname <- 'designmat6f_SAgeSexScanSoftIncEducPCsSBMI_females.txt' 
 outfile <- paste0(outpath, '/', fname) 
-catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion') 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completefemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
-# Design Matrix 7: brain ~ s(age) + sex + scan + soft + inc + educ + PCs + s(PDS) + s(BMI)
-fname <- 'designmat7_SAgeSexScanSoftIncEducPCsSPDSavgSBMI.txt' 
+# Design Matrix 7: brain ~ sex + scan + soft + inc + educ + PCs + s(PDS) + s(BMI)
+fname <- 'designmat7_SexScanSoftIncEducPCsSPDSavgSBMI.txt' 
 outfile <- paste0(outpath, '/', fname) 
 
 # Continuous variables
-contvar <- c(names(nda)[grep('bf_demean',names(nda))], names(nda)[grep('bf_pdsavg',names(nda))], names(nda)[grep('bf_bmi',names(nda))], paste0('PC',1:10), 'household.income_cont')
+contvar <- c('sex_cont', names(nda6)[grep('bf_pdsavg',names(nda6))], names(nda6)[grep('bf_bmi',names(nda6))], paste0('PC',1:10), 'household.income_cont')
 
 # Categorical variables
-catvar <- c('sex', 'high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
 
 # Note that default is set to demean=TRUE (demean continuous variables)
 makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
-# Design Matrix 7m: brain ~ s(age) + sex + scan + soft + inc + educ + PCs + s(PDS) + s(BMI) [MALES]
-fname <- 'designmat7m_SAgeSexScanSoftIncEducPCsSBMI_males.txt' 
+# Design Matrix 7m: brain ~ sex + scan + soft + inc + educ + PCs + s(PDS) + s(BMI) [MALES]
+fname <- 'designmat7m_SexScanSoftIncEducPCsSBMI_males.txt' 
 outfile <- paste0(outpath, '/', fname)
-catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion') 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
 
 ###############################################################
-# Design Matrix 7f: brain ~ s(age) + sex + scan + soft + inc + educ + PCs + s(PDS) + s(BMI) [FEMALES]
-fname <- 'designmat7f_SAgeSexScanSoftIncEducPCsSBMI_females.txt' 
+# Design Matrix 7f: brain ~ sex + scan + soft + inc + educ + PCs + s(PDS) + s(BMI) [FEMALES]
+fname <- 'designmat7f_SexScanSoftIncEducPCsSBMI_females.txt' 
 outfile <- paste0(outpath, '/', fname) 
-catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion') 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
+makeDesign(nda6_t1w_inc_completefemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
+
+###############################################################
+# Design Matrix 8: brain ~ s(age) + sex + scan + soft + inc + educ + PCs + s(PDS) + s(BMI)
+fname <- 'designmat8_SAgeSexScanSoftIncEducPCsSPDSavgSBMI.txt' 
+outfile <- paste0(outpath, '/', fname) 
+
+# Continuous variables
+contvar <- c(names(nda6)[grep('bf_demean',names(nda6))], 'sex_cont', names(nda6)[grep('bf_pdsavg',names(nda6))], names(nda6)[grep('bf_bmi',names(nda6))], paste0('PC',1:10), 'household.income_cont')
+
+# Categorical variables
+catvar <- c('high.educ', 'mri_info_deviceserialnumber', 'mri_info_softwareversion')
+
+# Note that default is set to demean=TRUE (demean continuous variables)
+tmp = makeDesign(nda6_t1w_inc_completecases, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
+
+###############################################################
+# Design Matrix 8m: brain ~ s(age) + sex + scan + soft + inc + educ + PCs + s(PDS) + s(BMI) [MALES]
+fname <- 'designmat8m_SAgeSexScanSoftIncEducPCsSBMI_males.txt' 
+outfile <- paste0(outpath, '/', fname)
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
+makeDesign(nda6_t1w_inc_completemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
+
+###############################################################
+# Design Matrix 8f: brain ~ s(age) + sex + scan + soft + inc + educ + PCs + s(PDS) + s(BMI) [FEMALES]
+fname <- 'designmat8f_SAgeSexScanSoftIncEducPCsSBMI_females.txt' 
+outfile <- paste0(outpath, '/', fname) 
+contvar <- contvar[which(!sapply(contvar, function(y) 'sex_cont' %in% y))]
 makeDesign(nda6_t1w_inc_completefemales, outfile, time, contvar=contvar, catvar=catvar, delta=NULL, interact=NULL, subjs=NULL, demean=TRUE, quadratic=NULL)
